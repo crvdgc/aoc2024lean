7af0f0b83b407c84f94167ba83693b1668ab4a46
crvdgc <ubikium@gmail.com>
2024-12-07 17:00:16 +0000

    day 7

--- Aoc2024lean/D7.lean
+++ Aoc2024lean/D7.lean
@@ -1,53 +1,40 @@
 import Aoc2024lean.Basic
-import Batteries.Data.MLList
 
 namespace D7
 
-open MLList
-
-abbrev L := MLList Id
-
-instance [ToString α]: ToString (L α) where
-  toString vs := vs.force.run.toString
-
-def parse (input : Array String) : Array (Int × List Int) :=
+def parse (input : Array String) : Array (Int × Array Int) :=
   input.map <| fun line =>
     let xs := line.splitOn ":"
     let test := xs[0]!.toInt!
-    let vs := xs[1]!.trim.splitOn |>.map String.toInt!
+    let vs := xs[1]!.trim.splitOn |>.map String.toInt! |>.toArray
     (test, vs)
 
-def isEmpty' (xs : L Int) : Bool :=
-  xs.uncons.run.isNone
+-- isMul
+def ops (n : Nat) : List (Array Bool) :=
+  match n with
+  | 0 => [Array.empty]
+  | n + 1 =>
+    let res := ops n
+    res.map (fun a => a.push true) ++ res.map (fun a => a.push false)
 
-partial def allVs (vs : L Int) : L Int := do
-  match vs.uncons.run with
-  | .none => .nil
-  | .some (v, vs) => do
-    if isEmpty' vs
-      then pure v
-      else
-        let isAdd ← MLList.ofList [true, false]
-        let (v' : Int) ← allVs vs
-        if isAdd
-          then pure (v + v')
-          else pure (v * v')
-
-def has (test : Int) (vs : L Int) : Bool :=
-  Id.run do
-    for v in vs do
-      if v == test then
-        return true
-    return false
-
-def getValid (test : Int) (vs : List Int) : Option Int :=
+def getValid (test : Int) (vs : Array Int) : Option Int :=
   let isValid :=
-    vs
-      |> MLList.ofList
-      |> allVs
-      |> dbgTraceVal
-      |> has test
-  if (dbgTraceVal isValid)
+    if vs.size == 0
+      then false
+      else
+        -- add trailing + 0
+        let ops := ops (vs.size - 1) |>.map (fun a => a.push false)
+        let vs := vs.push 0
+        let f (acc : Int) (p : Int × Bool) :=
+          let (v, isMul) := p
+          if isMul
+            then (acc * v)
+            else (acc + v)
+        ops.any <| fun ops =>
+          vs.tail.zip ops
+            |>.foldl f vs[0]!
+            |> (. = test)
+  if isValid
     then .some test
     else .none
 
@@ -57,9 +44,59 @@ def part1 (input : Array String) :=
     |>.filterMap (Function.uncurry getValid)
     |>.sum
   println! s!"{res}"
-  
+
+partial def facAux (acc : Int) (n : Int) :=
+  if n = 0
+    then acc
+    else
+      facAux (acc * 10) (n / 10)
+
+def fac (n : Int) : Int :=
+  facAux 1 n
+
+-- #eval fac 1
+-- #eval fac 43
+-- #eval fac 1325
+
+def concat (m : Int) (n : Int) : Int :=
+  m * fac n + n
+
+-- #eval concat 123 345
+
+def ops' (n : Nat) : List (Array (Int → Int → Int)) :=
+  match n with
+  | 0 => [Array.empty]
+  | n + 1 =>
+    let res := ops' n
+    res.map (fun a => a.push (. + .)) ++
+    res.map (fun a => a.push (. * .)) ++
+    res.map (fun a => a.push concat)
+
+def getValid' (test : Int) (vs : Array Int) : Option Int :=
+  let isValid :=
+    if vs.size == 0
+      then false
+      else
+        -- add trailing + 0
+        let ops := ops' (vs.size - 1) |>.map (fun a => a.push (. + .))
+        let vs := vs.push 0
+        let f (acc : Int) (p : Int × (Int → Int → Int)) :=
+          let (v, op) := p
+          op acc v
+        ops.any <| fun ops =>
+          vs.tail.zip ops
+            |>.foldl f vs[0]!
+            |> (. = test)
+  if isValid
+    then .some test
+    else .none
+
 def part2 (input : Array String) :=
-  println! s!"{input}"
+  let res := input
+    |> parse
+    |>.filterMap (Function.uncurry getValid')
+    |>.sum
+  println! s!"{res}"
 
 -- #eval (MLList.ofList [1]) |> isEmpty'
 
